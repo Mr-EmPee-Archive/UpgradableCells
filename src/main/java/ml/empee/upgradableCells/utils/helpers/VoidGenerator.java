4472397fa898b6eedcc3e5a147fafeaf57af7dae
Manuel Palumbo <manuel.palumbo@email.com>
2023-08-03 11:15:04 +0200

    Refactoring: Missing doc

--- src/main/java/ml/empee/upgradableCells/utils/helpers/VoidGenerator.java
+++ src/main/java/ml/empee/upgradableCells/utils/helpers/VoidGenerator.java
@@ -2,6 +2,10 @@ package ml.empee.upgradableCells.utils.helpers;
 
 import org.bukkit.generator.ChunkGenerator;
 
+/**
+ * Generator for a void world
+ */
+
 public class VoidGenerator extends ChunkGenerator {
 
 }
